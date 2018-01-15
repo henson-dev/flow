cb6b26bd0a7f1d5df60b1b196ff18d4ae92338e4
hensondev <hensondev@outlook.com>
2018-01-15 10:09:39 -0500

    added comments to example.js

--- examples/readme-example.js
+++ examples/readme-example.js
@@ -1,3 +1,5 @@
+/** Example from the README.md file */
+
 (async () => {
 
 const hf = require('henson-flow');
@@ -6,15 +8,15 @@ const assert = require('assert');
 let notFlat = [
     {
         id: 0,
-        res: [1, 2, 3],
+        res: [1, 2, 3], // Array as object property
     },
     {
         id: 1,
-        res: [2, 4, 5],
+        res: [2, 4, 5], // Array as object property
     },
 ];
 
-let flattened = await hf.yankFlat('res', notFlat);
+let flattened = await hf.yankFlat('res', notFlat); // [1, 2, 3, 2, 4, 5]
 
 assert([1, 2, 3, 2, 4, 5].join() === flattened.join(),
     'array should be flattened, and not re-ordered');
